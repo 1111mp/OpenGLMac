a9f3509cd0f4fdaf91390955cb783f1dd5b254d4
imp <zhangyifan@papegames.net>
2022-01-12 20:44:10 +0800

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by 张逸凡 on 2022/1/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
